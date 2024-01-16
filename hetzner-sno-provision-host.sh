db8b566270be223c55852c66e92bf6c95ec0987b
Pablo Alonso Rodriguez <palonsoro@gmail.com>
2024-01-16 15:50:58 +0100

    Replaced wget with curl

--- hetzner-sno-provision-host.sh
+++ hetzner-sno-provision-host.sh
@@ -7,6 +7,7 @@
 set -euo pipefail
 
 IPXE_SCRIPT_URL="${1}"
+curl -f -s -o discovery_ipxe_script.txt "${IPXE_SCRIPT_URL}"
 
 echo 'This script is meant to be run in the rescue environment to provision the Hetzner node so it can be discovered by assisted installer'
 
@@ -16,13 +17,11 @@ echo kexec-tools kexec-tools/load_kexec select true | debconf-set-selections
 
 apt-get install -y kexec-tools
 
-wget -O discovery_ipxe_script.txt "${IPXE_SCRIPT_URL}"
-
 INITRD_URL="$(awk '/^initrd/{print $NF}' discovery_ipxe_script.txt)"
 KERNEL_URL="$(awk '/^kernel/{print $2}' discovery_ipxe_script.txt)"
 KERNEL_CMDLINE="$(grep '^kernel' discovery_ipxe_script.txt  | cut -d' ' -f 3-)"
 
-wget -O kernel "${KERNEL_URL}"
-wget -O initrd "${INITRD_URL}"
+curl -f -s -o kernel "${KERNEL_URL}"
+curl -f -s -o initrd "${INITRD_URL}"
 
 kexec kernel --initrd=initrd --append="${KERNEL_CMDLINE}"
